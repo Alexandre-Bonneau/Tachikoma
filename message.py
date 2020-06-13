41cbcc0987b6e75c11dec608561d01164b7395a6
Alexandre Bonneau <alexandre.bonneau@laas.fr>
2020-06-13 20:15:50 +0200

    test

--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 #anti spam/cooldown
 import discord
 import random
-global test  = 0
+test  = 0
 def nosharp(str1):
     return str1.split("#")[0]
 
@@ -17,12 +17,12 @@ def query(author):
     return
 
 def message_function(m):
-
+    global test
     ret_value = []
     print(m.content)
     if "j'ai faim" in(m.content.lower()):
         test+=1
-        ret_value.append("T'as perdu : " +name(m.author))
+        ret_value.append("T'as perdu : " +name(m.author)+ str(test)
     if "bonjour" in(m.content.lower()):
         ret_value.append("Bonjour : " +name(m.author))
 
