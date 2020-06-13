f58aab88a8fab620d77e24c4bcecc9b68399869b
Alexandre Bonneau <alexandre.bonneau@laas.fr>
2020-06-13 19:53:02 +0200

    First files

---
+++ message.py
@@ -0,0 +1,53 @@
+#anti spam/cooldown
+import discord
+import random
+
+def nosharp(str1):
+    return str1.split("#")[0]
+
+def name(author):
+    if(type(author) is discord.user.User):
+        return nosharp(str(author))
+    if(type(author) is discord.member.Member):
+        return author.nick
+    return "noname"
+
+
+def query(author):
+    return
+
+def message_function(m):
+
+    ret_value = []
+    print(m.content)
+    if "j'ai faim" in(m.content.lower()):
+        print("42")
+        ret_value.append("T'as perdu : " +name(m.author))
+    if "bonjour" in(m.content.lower()):
+        ret_value.append("Bonjour : " +name(m.author))
+
+    if "$dé" in(m.content):
+
+        k=(m.content.split("$dé")[-1]).split("$")[0]
+        int(k)
+        rd = -1
+        # try:
+        value = int(k)
+
+        if value<=0:
+                ret_value.append("Les dés sont des entiers positifs")
+        else:
+            rd = random.randint(0,value-1)
+            if((rd ==0 or rd ==9) and value ==10):
+                rd2 = random.randint(0,value-1)
+                rd = 10*rd+rd2
+                if(rd>=96):
+                    ret_value.append("Oh le critique de " + name(m.author))
+                if(rd<=5):
+                    ret_value.append( "Cheh, critique de " + name(m.author))
+                    rd = "0"+str(rd)
+            ret_value.append( rd)
+        # except:
+        #     ret_value.append( "Les dés sont des entiers positifs")
+
+    return ret_value
---
+++ test2.py
@@ -0,0 +1,33 @@
+import discord
+import asyncio
+import random
+from bot_token import TOKEN
+from message import *
+random.seed()
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    tab = message_function(message)
+    print(tab)
+    if (len(tab)>0):
+        for m in tab:
+            print(m)
+            await message.channel.send(m)
+
+
+
+
+client.run(TOKEN)
+# cd /d D:
+# cd Programmation/Discordbot/Tachikoma
+# python test2.py
