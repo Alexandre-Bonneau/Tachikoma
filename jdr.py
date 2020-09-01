e334352c68c7a950e84b0c9d533126b62cb37822
Alexandre Bonneau <alexandre.bonneau@laas.fr>
2020-09-01 19:47:28 +0200

    dic

--- jdr.py
+++ jdr.py
@@ -8,4 +8,9 @@ def new_dic(for,dex,con,int,vol,cha):
     dic["dex"]=dex
     return dic
 
-# def dic_of_dic()
+def dic_of_dic():
+    dic = {}
+    dic[227088616575205376] = new_dic(4,8,6,10,6,10)
+    return dic
+
+# random.randint(1,dic[id][stat])
