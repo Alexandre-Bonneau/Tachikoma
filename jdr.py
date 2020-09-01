36ec468b979a7dbfd278ebeb221b6c55892e35c2
Alexandre Bonneau <alexandre.bonneau@laas.fr>
2020-09-01 19:30:32 +0200

    jdr dic

---
+++ jdr.py
@@ -0,0 +1,11 @@
+def new_dic(for,dex,con,int,vol,cha):
+    dic = {}
+    dic["for"]=for
+    dic["con"]=con
+    dic["int"]=ibt
+    dic["vol"]=vol
+    dic["cha"]=cha
+    dic["dex"]=dex
+    return dic
+
+# def dic_of_dic()
