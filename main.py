5df091f7e93bccb09e9249ebdf1375f6a7207e0e
Alexandre Bonneau <alexandre.bonneau@ens-rennes.fr>
2025-02-08 18:28:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,14 @@ async def on_message(message):
     if message.author == client.user:
         return
     tab = await message_function(message,call_together_ai)
+    print('rrr')
+    print(tab)
     if tab:
-            await message.channel.send(tab)
+        print("here")
+        
+        await message.channel.send("rrr")
+        await message.channel.send(tab)
+        
 
 
 
